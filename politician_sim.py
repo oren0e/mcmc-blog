9081c63d3bbe438f75f75df56dd50e9541d4dac8
Oren Epshtain <oren@gojuno.com>
2020-07-16 01:43:36 +0300

    finished the simulation.

--- politician_sim.py
+++ politician_sim.py
@@ -33,14 +33,17 @@ def run_mcmc(rounds: int, seed: int, islands: List[int]) -> List[int]:
     posterior: List[int] = [0 for _ in range(n)]
     round_count: int = 0
 
-    while round_count <= rounds:
-        print(round_count)
+    while round_count < rounds:
         # determine which direction to take
         proposal_string: str = random.choice(['left','right'])
         if proposal_string == 'right':
             proposal = current + 1
         elif proposal_string == 'left':
             proposal = current - 1
+
+        # condition for edge cases
+        if (proposal > n) or (proposal < 0):
+            proposal = 0
 
         p_move: float = min((proposal / current), 1)
         roulette: float = random.uniform(0, 1)
@@ -52,4 +55,10 @@ def run_mcmc(rounds: int, seed: int, islands: List[int]) -> List[int]:
 
     return posterior
 
-run_mcmc(rounds=1000, seed=SEED, islands=real_theta)
+post = run_mcmc(rounds=1000, seed=SEED, islands=real_theta)
+
+plt.bar([i for i in range(1, len(post) + 1)], post, color='brown')
+plt.xlabel(r'$\theta$')
+plt.ylabel('Frequency')
+plt.title('Posterior distribution of the population on the islands')
+plt.show()
